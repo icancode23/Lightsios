f802f50cfbe931af64200ba2ebfb9298950b6b77
nipunyo@gmail.com <nipunyo@gmail.com>
2016-10-10 21:45:54 +0530

    Initial Commit

---
+++ lights/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lights
+//
+//  Created by nipun arora on 10/10/16.
+//  Copyright © 2016 nipun arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lights/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lights
+//
+//  Created by nipun arora on 10/10/16.
+//  Copyright © 2016 nipun arora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
